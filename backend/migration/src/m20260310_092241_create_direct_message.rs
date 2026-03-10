6641abd453d8500f1ebf4d8d6ba1e5a26e0c7abc
dcyprien <cyprien.delmas@epitech.eu>
2026-03-10 11:54:47 +0100

    fix: direct_message model

--- backend/migration/src/m20260310_092241_create_direct_message.rs
+++ backend/migration/src/m20260310_092241_create_direct_message.rs
@@ -13,21 +13,21 @@ impl MigrationTrait for Migration {
                     .if_not_exists()
                     .col(ColumnDef::new(DirectMessages::Id).uuid().not_null().primary_key())
                     .col(ColumnDef::new(DirectMessages::Content).text().not_null())
-                    .col(ColumnDef::new(DirectMessages::SenderId).uuid().not_null())
-                    .col(ColumnDef::new(DirectMessages::ReceiverId).uuid().not_null())
+                    .col(ColumnDef::new(DirectMessages::User1Id).uuid().not_null()) // Remplacement
+                    .col(ColumnDef::new(DirectMessages::User2Id).uuid().not_null()) // Remplacement
                     .col(ColumnDef::new(DirectMessages::CreatedAt).timestamp_with_time_zone().not_null())
                     .foreign_key(
                         ForeignKey::create()
-                            .name("fk_direct_messages_sender_id")
-                            .from(DirectMessages::Table, DirectMessages::SenderId)
+                            .name("fk_direct_messages_user1_id")
+                            .from(DirectMessages::Table, DirectMessages::User1Id)
                             .to(Users::Table, Users::Id)
                             .on_delete(ForeignKeyAction::Cascade)
                             .on_update(ForeignKeyAction::Cascade),
                     )
                     .foreign_key(
                         ForeignKey::create()
-                            .name("fk_direct_messages_receiver_id")
-                            .from(DirectMessages::Table, DirectMessages::ReceiverId)
+                            .name("fk_direct_messages_user2_id")
+                            .from(DirectMessages::Table, DirectMessages::User2Id)
                             .to(Users::Table, Users::Id)
                             .on_delete(ForeignKeyAction::Cascade)
                             .on_update(ForeignKeyAction::Cascade),
@@ -50,8 +50,8 @@ enum DirectMessages {
     Table,
     Id,
     Content,
-    SenderId,
-    ReceiverId,
+    User1Id, // Remplacement
+    User2Id, // Remplacement
     CreatedAt,
 }
 
